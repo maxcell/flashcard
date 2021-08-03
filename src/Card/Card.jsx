dddcf75cc1310f0caba055f768efb17e0fa39bb9
Prince Wilson <maxcell.wilson@gmail.com>
2021-08-03 18:58:27 -0400

    Implements API for Card component

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,7 +1,26 @@
+
+export const states = {
+  front: "FRONT",
+  back: "BACK"
+}
+
 export const Card = (props) => {
+  const {
+    front,
+    back,
+    displaySide = states.front
+  } = props;
+
   return (
     <div>
-      Hello!
+      {displaySide === states.front && front}
+      {displaySide === states.back && back}
     </div>
   )
 }
+/*
+  front
+  back
+  sideToDisplay
+
+*/
--- src/Card/Card.stories.js
+++ src/Card/Card.stories.js
@@ -1,10 +1,23 @@
-import {Card} from './Card'
+import {Card, states as CardStates} from './Card'
+import {Fragment, useState} from 'react'
 
 export default {
   title: 'Example/Card',
   component: Card,
 };
 
-export const SimpleCard = (props) => {
-  return (<Card />)
+export const SimpleCard = (args) => {
+  const [displaySide, setDisplaySide] = useState(CardStates.front)
+  return (
+    <Fragment>
+      <Card {...args} displaySide={displaySide}/>
+      <button onClick={() => setDisplaySide(CardStates.front)}>Flip to Front</button>
+      <button onClick={() => setDisplaySide(CardStates.back)}>Flip to Back</button>
+    </Fragment>
+  )
+}
+
+SimpleCard.args = {
+  front: "Beef",
+  back: "Not Beef"
 }
